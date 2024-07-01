a67a45ee2d23c6971007b43bbc29a3ac9df9c061
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-07-01 17:05:15 -0500

    add assets

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,4 +1,12 @@
-import unknownArtist from '@/assets/unknown-artist.png'
+import unknown_artist from '@/assets/unknown_artist.png'
+import unknown_track from '@/assets/unknown_track.png'
+
+import { Image } from 'react-native'
+
+const unknown_image_track_uri = Image.resolveAssetSource(unknown_track).uri
+const unknown_image_artist_uri = Image.resolveAssetSource(unknown_artist).uri
+
 export const assets = {
-	unknownArtist,
+	unknown_image_track_uri,
+	unknown_image_artist_uri,
 }
